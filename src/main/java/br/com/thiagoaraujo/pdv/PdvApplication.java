20a3a37c6148fddd6ca7aac032a6c4da52753be2
thiago <thiago.santosbhz@gmail.com>
2023-03-29 22:34:29 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/thiagoaraujo/pdv/PdvApplication.java
@@ -0,0 +1,15 @@
+package br.com.thiagoaraujo.pdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvApplication.class, args);
+		
+		
+	}
+
+}
---
+++ src/test/java/br/com/thiagoaraujo/pdv/PdvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagoaraujo.pdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
